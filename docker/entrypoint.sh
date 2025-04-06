c67b5bfc2ef9b9c09146d73d4d47950372be92ed
Hammad Usmani <ham.u@live.com>
2025-04-06 00:46:48 +0000

    Change cron job definition for new cli framework. #37

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -15,7 +15,7 @@ nohup python /hurricane-deploy/run.py > logs.txt &
 # Setup a cron schedule
 echo "SHELL=/bin/bash
 BASH_ENV=/container.env
-0 * * * * python /hurricane-deploy/report.py >> /var/log/cron.log 2>&1
+0 * * * * python /hurricane-deploy/report.py hourly >> /var/log/cron.log 2>&1
 # This extra line makes it a valid cron" > scheduler.txt
 
 crontab scheduler.txt
