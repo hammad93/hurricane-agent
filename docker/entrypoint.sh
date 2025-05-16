5d3cde3d596b5b92d0e60ce360b39e6e14bb50de
Hammad Usmani <ham.u@live.com>
2025-05-16 02:46:00 +0000

    Misc bug fixes and improvements

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,10 +5,11 @@ echo "Docker container has been started"
 declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
 
 # setup git
+cd /hurricane-agent/
 git config --local --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
 
 # run the tensorflow server
-python /hurricane-deploy/download_models.py
+python /hurricane-agent/download_models.py
 nohup tensorflow_model_server --model_base_path=/root/forecast --rest_api_port=9000 --model_name=hurricane 2>&1 &
 
 # run the API
