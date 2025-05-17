f3dd24b8a02c2cd12e78e06bba2edb5a272c5a7f
Hammad Usmani <ham.u@live.com>
2025-05-17 04:11:17 +0000

    Build on blank template based on report.py. References #37

--- agent/daily.py
+++ agent/daily.py
@@ -10,4 +10,14 @@ def transform_data(data):
 
 def create_report(data):
     '''
+    data is based on this query or similar
+    SELECT * FROM ingest_hash
+    WHERE time >= NOW() - INTERVAL '24 hours';
     '''
+    result = {
+        'BODY_TEXT': None,
+        'BODY_HTML': None,
+        'RECIPIENTS': 'daily@fluids.ai',
+        'SUBJECT': 'fluids hurricane agent: Daily Reports'
+    }
+    return result
