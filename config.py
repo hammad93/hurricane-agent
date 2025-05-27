f2d0e5da5207a9c39c773fed3db688c71b42bfad
Hammad Usmani <ham.u@live.com>
2025-05-27 00:24:23 -0400

    Update config.py with log dir

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-import utils
 feature_scaler_path='https://github.com/apatel726/HurricaneDissertation/raw/master/hurricane_ai/scaler/feature_scaler.pkl'
 forecast_model='https://storage.googleapis.com/cyclone-ai.appspot.com/model_2021_09_23_03_27/saved_model.pb'
 forecast_chatgpt='https://raw.githubusercontent.com/hammad93/hurricane-net/main/hurricane_net_chatgpt.py'
@@ -17,5 +16,5 @@ daily_prompt_path = 'docs/daily_report_ingest_prompt.txt'
 fluids_url = 'https://fluids.ai/'
 wp_create_post_url=f'{fluids_url}wp-json/wp/v2/posts'
 gpt_model = '/data/phi-4.gguf'
-agent_log_path = f'{utils.get_script_dir()}/logs'
+agent_log_path = f'/hurricane-agent/logs'
 agent_log_format = '%(asctime)s - %(levelname)s - %(message)s'
